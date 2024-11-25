010eb7c6e43ddbe11664dcc7d566dee3646f5531
dayane112 <71454308+dayane112@users.noreply.github.com>
2024-11-24 19:54:12 -0500

    Change the icon inverted

--- src/pages/AddRecipe.jsx
+++ src/pages/AddRecipe.jsx
@@ -22,11 +22,13 @@ function AddRecipe() {
     return (
         <>
             <h1> RECIPE<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="45" height="45" color="#39906d" fill="none" >
+            <g transform="scale(-1, 1) translate(-24, 0)">
                 <path d="M21 17C18.2386 17 16 14.7614 16 12C16 9.23858 18.2386 7 21 7" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
                 <path d="M21 21C16.0294 21 12 16.9706 12 12C12 7.02944 16.0294 3 21 3" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
                 <path d="M6 3L6 8M6 21L6 11" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                 <path d="M3.5 8H8.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                 <path d="M9 3L9 7.35224C9 12.216 3 12.2159 3 7.35207L3 3" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                </g>
             </svg></h1>
             <form className="recipeForm" onSubmit={handleSubmit}>
                 <div className="form-group">
